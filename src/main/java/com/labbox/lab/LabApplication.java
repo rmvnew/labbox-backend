112ec6bcd4ace6d78b5fbc827cb66821fc95251b
Ricardo <rmvnew@gmail.com>
2023-04-01 07:18:49 -0400

    chore: add .env and configuration of database

--- src/main/java/com/labbox/lab/LabApplication.java
+++ src/main/java/com/labbox/lab/LabApplication.java
@@ -2,8 +2,10 @@ package com.labbox.lab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("file:/home/ricardo/Project/API/JAVA/labbox-backend/.env")
 public class LabApplication {
 
 	public static void main(String[] args) {
