1af3d9b7ac079e0e79b0636092015e753328aafe
Ricardo <rmvnew@gmail.com>
2023-04-03 09:34:42 -0400

    chore: add custonException

---
+++ src/main/java/com/labbox/lab/exception/CustomException.java
@@ -0,0 +1,21 @@
+package com.labbox.lab.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class CustomException extends RuntimeException {
+    private HttpStatus status;
+    private String message;
+
+    public CustomException(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
